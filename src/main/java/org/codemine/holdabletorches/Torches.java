e65c61e3d5ac34bff5bcb58ebaa29bb6b33267e3
Relicum <chrislutte@relicum.com>
2014-03-21 17:46:18 +0000

    Initial commit

---
+++ src/main/java/org/codemine/holdabletorches/Torches.java
@@ -0,0 +1,23 @@
+package org.codemine.holdabletorches;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Name: Torches.java Created: 21 March 2014
+ *
+ * @author Relicum
+ * @version 0.0.1
+ */
+public class Torches extends JavaPlugin {
+
+    public void onEnable() {
+
+
+    }
+
+    public void onDisable() {
+
+
+    }
+
+}
